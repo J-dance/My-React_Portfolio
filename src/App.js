66c25872def2a233d792833d1cef92a55e118627
J-dance <dance.jojo@gmail.com>
2021-04-01 20:50:12 +0100

    React-router-dom setup to link between pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
     
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+    
   );
 }
 
