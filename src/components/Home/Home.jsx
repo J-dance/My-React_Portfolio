4753847e54c7314ba150885f2cef168fa99a985a
J-dance <dance.jojo@gmail.com>
2021-04-03 14:12:30 +0100

    landing page animation started

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from "framer-motion";
 import styles from "./Home.module.scss";
 import NavBar from "../NavBar";
-import { introVariant, hoverVariant, componentTransition } from "../../variants";
+import { introVariant, hoverVariant, componentTransition, hiVariant, nameVariant } from "../../variants";
 
 
 function Home() {
@@ -20,19 +20,38 @@ function Home() {
           whileHover="whileHover"
         >Hello.</motion.h1>
       </header>
-      <main className={styles.main}>
-        <motion.div
-          variants={introVariant}
-          initial="initial"
-          animate="animate"
-          transition={{
-            duration: 5,
-            delay: 2,
-            times: [0, 0.4, 0.7, 1]
-          }}
-        >
-          <div className={styles.container}></div>
-        </motion.div>
+      <main className={styles.main}>          
+          <motion.div className={styles.container}
+            variants={introVariant}
+            initial="initial"
+            animate="animate"
+            transition={{
+              duration: 5,
+              delay: 2,
+              times: [0, 0.4, 0.7, 1]
+            }}
+          >
+            <motion.h2
+              variants={hiVariant}
+              initial='hidden'
+              animate='visible'
+              transition={{
+                duration: 3,
+                delay: 4,
+                times: [0, 0.6, 0.8, 1]
+              }}
+            >Hi</motion.h2>
+            <motion.h2
+              variants={nameVariant}
+              initial='hidden'
+              animate='visible'
+              transition={{
+                duration: 3,
+                delay: 5,
+                // times: [0, 0.4, 1]
+              }}
+            >i'm jojo</motion.h2>
+          </motion.div>
       </main>
     </motion.div>
   )
--- src/variants.js
+++ src/variants.js
@@ -2,16 +2,44 @@ import React from "react";
 
 export const introVariant = {
   initial: {
+    borderRadius: "50%",
+    width: 100,
+    height: 100,
     scale: 1,
     x: 0,
     y: 0
   },
   animate: {
+    width: [100, 100, 100, 150],
+    height: [100, 100, 100, 70],
     scale: [1, 2, 2, 2],
-    x: [0, -200, -200, 100],
-    y: [0, -200, -200, -200]
-  },
+    x: [0, -200, -200, 150],
+    y: [0, -200, -200, -200],
+    borderRadius: ["50%", "50%", "50%", "30px"]
+  }
   
+}
+
+export const hiVariant = {
+  hidden: {
+    opacity: 0,
+    x: 40
+  },
+  visible: {
+    opacity: [0, 1, 1, 0],
+    x: [40, 40, -100, -100]
+  }
+}
+
+export const nameVariant = {
+  hidden: {
+    opacity: 0,
+    x: 200
+  },
+  visible: {
+    opacity: 1,
+    x: 0
+  }
 }
 
 export const hoverVariant ={
