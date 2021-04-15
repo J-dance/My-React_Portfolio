8762bdaf733f90fcfca4d67e7acb1535ca060d89
J-dance <dance.jojo@gmail.com>
2021-04-15 14:31:07 +0100

    footer completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import styles from "./Footer.module.scss";
+import Logo from "../../assets/SVGs/logo.svg";
+import github from "../../assets/SVGs/github-svgrepo-com.svg";
+import linkedin from "../../assets/SVGs/linkedin-svgrepo-com.svg";
+
+
 
 const Footer = () => {
   return (
     <footer>
-    
+    <img className={styles.logo} src={Logo}></img>
+    <a href="https://github.com/J-dance?tab=repositories" target="_blank">
+      <img className={styles.icon} src={github}></img>
+    </a>
+    <a href="https://www.linkedin.com/in/jojo-dance/" target="_blank">
+      <img className={styles.icon} src={linkedin}></img>
+    </a>
+    <p>Jojo Dance | 2021</p>
     </footer>
   )
 }
